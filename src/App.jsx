cee74b8eb2a329ab07175f249b04d600cff1c4dc
Vishal More <vishalmore4821@gmail.com>
2025-10-18 02:00:18 +0530

    Third commit, added about,projects,,testimonials,and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
-
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
+import Contact from './components/Contact'
+import { ToastContainer } from 'react-toastify';
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div className='w-full overflow-hidden'>
-
+    <div className='w-full overflow-x-hidden'>
+      <ToastContainer/>
       <Header />
+      <About/>
+      <Projects/>
+      <Testimonails/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,11 +21,11 @@ const Navbar = () => {
                 <ul className='hidden md:flex gap-7 text-white'>
                     <a href="#Header" className='cursor-pointer 
                 hover:text-gray-400'>Home</a>
-                    <a href="#Header" className='cursor-pointer 
+                    <a href="#About" className='cursor-pointer 
                 hover:text-gray-400'>About</a>
-                    <a href="#Header" className='cursor-pointer 
+                    <a href="#Projects" className='cursor-pointer 
                 hover:text-gray-400'>Projects</a>
-                    <a href="#Header" className='cursor-pointer 
+                    <a href="#Testimonails" className='cursor-pointer 
                 hover:text-gray-400'>Testimonials</a>
                 </ul>
                 <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
